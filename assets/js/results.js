7c1f4988a3315dfb0232ec94e071c7fea89ae10f
Jbrockhoff <150721892+Jbrockhoff@users.noreply.github.com>
2024-01-31 21:28:19 -0600

    began search/results functions

--- assets/js/results.js
+++ assets/js/results.js
@@ -2,7 +2,31 @@
 var API_KEY = "AIzaSyCS9ZrJOwXU9eQOeUGoLmH5TcdW-5xT8RE"
 
 function search() {
-  var searches = document.getElementsByClassName("btn btn-ghost btn-circle")
-}
+//   gapi.client.setApiKey('AIzaSyCS9ZrJOwXU9eQOeUGoLmH5TcdW-5xT8RE');
+//   gapi.client.load('youtube', 'v3', function(){
+//           makeRequest();
+//   });
+// }
+// function makeRequest(){
+//   var q = $('#query').val();
+//   var request = gapi.client.youtube.search.list({
+//           q: q,
+//           part: 'snippet', 
+//           maxResults: 10
+//   });
+//   request.execute(function(response)  {                                                                                    
+//           $('#results').empty()
+//           var srchItems = response.result.items;                      
+//           $.each(srchItems, function(index, item){
+//           vidTitle = item.snippet.title;  
+//           vidThumburl =  item.snippet.thumbnails.default.url;                 
+//           vidThumbimg = '<pre><img id="thumb" src="'+vidThumburl+'" alt="No  Image  Available." style="width:204px;height:128px"></pre>';                   
+
+//           $('#results').append('<pre>' + vidTitle + vidThumbimg +   '</pre>');                      
+//   })  
+// })  
+ }
+
 
 searchButton.addEventListener("click", search)
+
