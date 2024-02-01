bdf5920e6225555eac3853eadc04fa81c2b2dcbb
Jbrockhoff <150721892+Jbrockhoff@users.noreply.github.com>
2024-01-31 18:24:36 -0600

    embedded youtube in html, commented out JS because not functional

--- assets/js/youtube.js
+++ assets/js/youtube.js
@@ -1,17 +1,17 @@
-const frame = document.getElementById("ytEmbed");
-const channelID = "UCS6s3OidfTU0fMRScTGE_jg";
-var guid="";
-var APIKey = "AIzaSyCS9ZrJOwXU9eQOeUGoLmH5TcdW-5xT8RE"
+// const frame = document.getElementById("ytEmbed");
+// const channelID = "UCS6s3OidfTU0fMRScTGE_jg";
+// var guid="";
+// var APIKey = "AIzaSyCS9ZrJOwXU9eQOeUGoLmH5TcdW-5xT8RE"
 
 
-$(window).on("load", function() {   
-fetch("https://api.rss2json.com/v1/api.json?rss_url=" + encodeURIComponent("https://www.youtube.com/feeds/videos.xml?channel_id=" + channelID))
-    .then(response => response.json())
-    .then(data => {
-        guid = data["items"][0]["guid"]
-        const embedURL = "https://youtube.com/embed/" + guid.replace("yt:video:", "")
-        frame.src = embedURL;
-    })
-    .catch(console.error);     
-    console.log( "ready!" );
-});           
+// $(window).on("load", function() {   
+// fetch("https://api.rss2json.com/v1/api.json?rss_url=" + encodeURIComponent("https://www.youtube.com/feeds/videos.xml?channel_id=" + channelID))
+//     .then(response => response.json())
+//     .then(data => {
+//         guid = data["items"][0]["guid"]
+//         const embedURL = "https://youtube.com/embed/" + guid.replace("yt:video:", "")
+//         // frame.src = embedURL;
+//     })
+//     .catch(console.error);     
+//     console.log( "ready!" );
+// });           
