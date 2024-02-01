5eb9d7362ced40076730ffdbaa01379f18216e8e
AlexanderJGael <130517352+AlexanderJGael@users.noreply.github.com>
2024-01-31 18:54:16 -0600

    updated youtube js

--- assets/js/youtube.js
+++ assets/js/youtube.js
@@ -1,17 +1,11 @@
-const frame = document.getElementById("ytEmbed");
-const channelID = "UCS6s3OidfTU0fMRScTGE_jg";
-var guid="";
-var APIKey = "AIzaSyCS9ZrJOwXU9eQOeUGoLmH5TcdW-5xT8RE"
+var API_KEY = "AIzaSyCS9ZrJOwXU9eQOeUGoLmH5TcdW-5xT8RE";
+var CHANNEL_ID = "UCS6s3OidfTU0fMRScTGE_jg";
 
-
-$(window).on("load", function() {   
-fetch("https://api.rss2json.com/v1/api.json?rss_url=" + encodeURIComponent("https://www.youtube.com/feeds/videos.xml?channel_id=" + channelID))
-    .then(response => response.json())
-    .then(data => {
-        guid = data["items"][0]["guid"]
-        const embedURL = "https://youtube.com/embed/" + guid.replace("yt:video:", "")
-        frame.src = embedURL;
-    })
-    .catch(console.error);     
-    console.log( "ready!" );
-});           
+fetch(`https://www.googleapis.com/youtube/v3/search?key=${API_KEY}&channelId=${CHANNEL_ID}&part=snippet,id&order=date&maxResults=1`)
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data)
+    let videoId = data.items[0].id.videoId;
+    document.getElementById('youtubeEmbed').innerHTML = `<iframe width="560" height="315" src="https://www.youtube.com/embed/${videoId}" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`;
+  })
+  .catch((error) => console.error('Error:', error));
