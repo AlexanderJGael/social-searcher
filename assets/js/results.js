1d79bd230df92b502793d20cee1dfb123f0252c9
Jbrockhoff <150721892+Jbrockhoff@users.noreply.github.com>
2024-02-01 19:52:24 -0600

    linked ajax and ApiClientReady scripts to HTML

--- assets/js/results.js
+++ assets/js/results.js
@@ -1,6 +1,7 @@
 //get results, send array back to html
-var API_KEY = "AIzaSyCS9ZrJOwXU9eQOeUGoLmH5TcdW-5xT8RE"
-var searchBtn = document.getElementById("search-btn")                        
+// var API_KEY = "AIzaSyCS9ZrJOwXU9eQOeUGoLmH5TcdW-5xT8RE"
+var searchBtn = document.getElementById("search-btn")  
+
 function search() {
   gapi.client.setApiKey('AIzaSyCS9ZrJOwXU9eQOeUGoLmH5TcdW-5xT8RE');
   gapi.client.load('youtube', 'v3', function(){
@@ -13,9 +14,9 @@ function makeRequest(){
   var request = gapi.client.youtube.search.list({
           q: search,
           part: 'snippet', 
-          maxResults: 10,
-          
-  });
+          maxResults: 10,   
+   }); 
+  
   request.execute(function(response)  {                                                                                    
           $('#results').empty()
           var srchItems = response.result.items;                      
@@ -24,11 +25,11 @@ function makeRequest(){
           vidThumburl =  item.snippet.thumbnails.default.url;                 
           vidThumbimg = '<pre><img id="thumb" src="'+vidThumburl+'" alt="No  Image  Available." style="width:204px;height:128px"></pre>';                   
 
-          $('#results').append('<pre>' + vidTitle + vidThumbimg +   '</pre>');                      
-  })  
-})  
+          $('#results').append('<pre>' + vidTitle + vidThumbimg + '</pre>');                  
+      })  
+    })  
  }
-
+//add if statement for displaying search results on page
 
 searchBtn.addEventListener("click", search)
 
