267b8b46ef9e87fbcb33085c68f55b58fa14224b
Jbrockhoff <150721892+Jbrockhoff@users.noreply.github.com>
2024-02-01 19:30:09 -0600

    added HTML for search results (need placement). Refined results.js, need more API keys

--- assets/js/results.js
+++ assets/js/results.js
@@ -1,32 +1,34 @@
 //get results, send array back to html
-// var API_KEY = "AIzaSyCS9ZrJOwXU9eQOeUGoLmH5TcdW-5xT8RE"
+var API_KEY = "AIzaSyCS9ZrJOwXU9eQOeUGoLmH5TcdW-5xT8RE"
+var searchBtn = document.getElementById("search-btn")                        
+function search() {
+  gapi.client.setApiKey('AIzaSyCS9ZrJOwXU9eQOeUGoLmH5TcdW-5xT8RE');
+  gapi.client.load('youtube', 'v3', function(){
+          makeRequest();
+  });
+}
+                       
+function makeRequest(){
+  var search = $('#search').val();
+  var request = gapi.client.youtube.search.list({
+          q: search,
+          part: 'snippet', 
+          maxResults: 10,
+          
+  });
+  request.execute(function(response)  {                                                                                    
+          $('#results').empty()
+          var srchItems = response.result.items;                      
+          $.each(srchItems, function(index, item){
+          vidTitle = item.snippet.title;  
+          vidThumburl =  item.snippet.thumbnails.default.url;                 
+          vidThumbimg = '<pre><img id="thumb" src="'+vidThumburl+'" alt="No  Image  Available." style="width:204px;height:128px"></pre>';                   
 
-// function search() {
-//   gapi.client.setApiKey('AIzaSyCS9ZrJOwXU9eQOeUGoLmH5TcdW-5xT8RE');
-//   gapi.client.load('youtube', 'v3', function(){
-//           makeRequest();
-//   });
-// }
-// function makeRequest(){
-//   var q = $('#query').val();
-//   var request = gapi.client.youtube.search.list({
-//           q: q,
-//           part: 'snippet', 
-//           maxResults: 10
-//   });
-//   request.execute(function(response)  {                                                                                    
-//           $('#results').empty()
-//           var srchItems = response.result.items;                      
-//           $.each(srchItems, function(index, item){
-//           vidTitle = item.snippet.title;  
-//           vidThumburl =  item.snippet.thumbnails.default.url;                 
-//           vidThumbimg = '<pre><img id="thumb" src="'+vidThumburl+'" alt="No  Image  Available." style="width:204px;height:128px"></pre>';                   
-
-//           $('#results').append('<pre>' + vidTitle + vidThumbimg +   '</pre>');                      
-//   })  
-// })  
-//  }
+          $('#results').append('<pre>' + vidTitle + vidThumbimg +   '</pre>');                      
+  })  
+})  
+ }
 
 
-// searchButton.addEventListener("click", search)
+searchBtn.addEventListener("click", search)
 
