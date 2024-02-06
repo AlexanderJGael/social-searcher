3f406413f44c44085968dc15fd15a52cfb019b00
AlexanderJGael <130517352+AlexanderJGael@users.noreply.github.com>
2024-02-05 21:30:55 -0600

    working on results link

--- assets/js/search.js
+++ assets/js/search.js
@@ -3,6 +3,6 @@ document.getElementById('search-btn').addEventListener('click', function(event){
     event.preventDefault()
     var search = document.getElementById("search").value
     localStorage.setItem("search", search)
-    window.location.replace("results.html")
+    window.location.href = "../html/results.html";
 })
 
